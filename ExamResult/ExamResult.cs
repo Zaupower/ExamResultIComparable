df43a8c80dd38cf1dde7d0bfa6714757f2b5cc78
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-11-08 15:11:13 +0000

    added enums and class, implemented IComparable<ExamResult>

---
+++ ExamResult/ExamResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamResult
+{
+    public class ExamResult : IComparable<ExamResult>
+    {
+        int ID;
+        string Name;
+        Exams exam;
+        Score score;
+        DateTime date;
+
+        public int CompareTo(ExamResult other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExamResult/Exams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamResult
+{
+    public enum Exams
+    {
+        ENGLISH,
+        MATHEMATICS,
+        PHYSICS
+
+    }
+}
